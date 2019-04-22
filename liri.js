432270888aa6bd665583e079a93302cba8e10bc9
Rishabh Goel <Rishabh@rishabhs-mbp.lan>
2019-04-22 17:53:44 -0400

    v1

--- liri.js
+++ liri.js
@@ -1,5 +1,71 @@
 require("dotenv").config();
-
+// create variables for required items
 var keys = require("./keys.js");
-
+var Spotify = require("node-spotify-api");
+var axios = require("axios");
+var fs = require("fs");
+var moment = require("moment");
+// create variable to access spotify key
 var spotify = new Spotify(keys.spotify);
+// take in two args
+var action = process.argv[2];
+var parameter = process.argv[3];
+// create a function with switch/case inside to decide which function will be run 
+function whichAction() {
+  switch (action) {
+    case "concert-this":
+      bandSearch(parameter);
+      break;
+
+    case "spotify-this-song":
+      songInfo(parameter);
+      break;
+
+    case "movie-this":
+      movieInfo(parameter);
+      break;
+
+    case "do-what-it-says":
+      iWantItThatWay();
+      break;
+
+    default:
+      console.log("Command Not Found! Please choose from the following options: concert-this, spotify-this-song, movie-this, do-what-it-says");
+      break;
+  }
+};
+// create a function to run if action === concert-this
+function bandSearch(parameter) {
+  if (action === "concert-this") {
+    var artist = "";
+    for (var i = 3; i < process.argv.length; i++) {
+      artist += process.argv[i];
+    }
+    console.log(artist);
+  } else {
+    artist = parameter;
+  }
+  // create variable for queryURL for bandsInTown
+  var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+  // console log queryURL
+  console.log(queryURL);
+  // run a request with axios to bandsInTown 
+  axios.get(queryURL).then(
+    function (response) {
+      // create variable to store stringified response 
+      var data = JSON.stringify(response);
+        // get venue name
+        console.log("Venue: " + data[i].venue.name);
+        // get venue location
+        console.log("Location: " + data[i].venue.city);
+        // get date of show and convert using moment
+        var date = data[i].datetime;
+        date = moment(date).format("MM/DD/YYYY");
+        console.log("Date: " + date);
+    }
+  );
+};
+
+whichAction();
+
+// ERRORS WITH CODE: RUNS CONSOLE LOG ON LINE 51 BUT THEN ERROR OCCURS IN AXIOS CALL. TYPE ERROR: CONVERTING CIRCULAR STRUCTURE TO JSON
